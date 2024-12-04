3f276ac7e2d5b3036e6abf8c198bbf888ca4245b
giorgia01carboni <giorgia.carboni@gmail.com>
2024-12-04 22:28:50 +0100

    first 2 points updated

---
+++ autoregressive_model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+N = 100
+w = np.random.randn(N)
+# coefficients of the autoregressive part
+a = [1.0, 0.5, -0.5, -0.3]
+
+# y_k -> realizations.
+# Find every value of y_k using previous values y_k-1 and
+# gaussian white noise w_k
+
+y =
+
+# use lflilter for the difference equations
